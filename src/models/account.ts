54a0ba736380a60ccf6cb42aa1d3d8fcccb9692b
Mike Rogers <mtr293@gmail.com>
2023-09-18 23:00:27 -0400

    add data models

---
+++ src/models/account.ts
@@ -0,0 +1,7 @@
+export type Account = {
+  id: string
+  username: string
+  email: string
+  createdAt: Date
+  updatedAt: Date
+}
---
+++ src/models/hero.ts
@@ -0,0 +1,20 @@
+export enum HeroClass {
+  Warrior = 'WARRIOR',
+  Thief = 'THIEF',
+  Wizard = 'WIZARD',
+  Cleric = 'CLERIC',
+  Ranger = 'RANGER'
+
+}
+
+
+export type Hero = {
+  id: string
+  accountId: string
+  name: string
+  level: number
+  class: HeroClass
+  experience: number
+  hiredAt: Date
+  diedAt?: Date
+}
